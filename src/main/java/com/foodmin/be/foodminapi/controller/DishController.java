fd53ceacf784a8d7bf6107403c38153ec2ff36d0
Christian Geissler <christian.cg.geissler@gmail.com>
2021-01-09 21:15:09 +0100

    fixed Rest controller "dish"

--- src/main/java/com/foodmin/be/foodminapi/controller/DishController.java
+++ src/main/java/com/foodmin/be/foodminapi/controller/DishController.java
@@ -4,9 +4,11 @@ import com.foodmin.be.foodminapi.model.Dish;
 import com.foodmin.be.foodminapi.repository.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class DishController {
 
     @Autowired
